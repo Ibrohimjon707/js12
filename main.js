0088f156a3ccf959d316e110ea4ea26248a6746e
Ibrohimjon707 <ibrohimjonathamjonov38@gmail.com>
2025-01-29 13:50:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let ism = prompt("Ismingizni kiriting")
+let id = prompt("Id kiriting")
+let pass = prompt("Password kiriting")
+let object = {
+    name: ism,
+    id: id,
+ password: pass,
+};
+// console.log(ism);
+// console.log(id);
+// console.log(pass);
+
+
+console.log(object);
